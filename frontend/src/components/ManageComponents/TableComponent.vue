f84f9cc932e299a3d3eb42e172cec79f489b3d68
Marco de Boer <128293216+GitMarcoo@users.noreply.github.com>
2024-01-02 12:45:46 +0100

    Update index.html and TableComponent.vue, add flowbite.min.js and expense functionality

--- frontend/src/components/ManageComponents/TableComponent.vue
+++ frontend/src/components/ManageComponents/TableComponent.vue
@@ -13,7 +13,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="(value, key) in data" :key="key" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                    <tr v-for="(value, key) in expenses" :key="key" class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             {{ value.name }}
                         </th>
@@ -25,14 +25,16 @@
             </table>
         </div>
         <div class="flex justify-end bg-transparent mt-2">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <button @click="addExpense" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Add
             </button>
         </div>
     </div>
 </template>
 <script setup lang="ts">
-import { defineProps, computed } from 'vue';
+import { defineProps, computed, ref } from 'vue';
+import Expense from '@/models/Expense';
+
 const props = defineProps({
     title: {
         type: String,
@@ -43,6 +45,12 @@ const props = defineProps({
         required: true
     }
 });
+
+const expenses = ref(props.data as Expense[]);
+
+const addExpense = () => {
+    expenses.value.push(new Expense('test', 10, 'MONTHLY'));
+}
 
 const totalAmount = computed(() => {
     return props.data.reduce((acc, curr) => acc + curr.amount, 0);
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -14,13 +14,13 @@
             </button>
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                    <router-link  to="/" class="navItem " active-class="" aria-current="page">Home</router-link>
+                <li class="self-center">
+                    <router-link  to="/" class="navItem" aria-current="page">Home</router-link>
                 </li>
-                <li>
+                <li class="self-center">
                     <router-link  to="/about" class="navItem">About</router-link>
                 </li>
-                <li>
+                <li class="self-center">
                     <router-link  to="/manage" class="navItem ">Manage</router-link>
                 </li>
                 <li>
@@ -33,7 +33,6 @@
                         </svg>
                     </button>
                 </li>
-
             </ul>
             </div>
         </div>
